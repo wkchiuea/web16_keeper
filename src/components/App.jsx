518722ca52d6985d3ee0f9432407752e7cc8ba32
wkchiuea <103587188+wkchiuea@users.noreply.github.com>
2022-06-29 21:17:46 +0800

    implement add note functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [itemList, setItemList] = useState([]);
+
+  function addItem(item) {
+    setItemList((preItemList) => {
+      return [...preItemList, item];
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea addItem={addItem} />
+      {itemList.map((item, i) => {
+        return <Note key={i} title={item.title} content={item.content} />
+      })}
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [newItem, setNewItem] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleInputField(event) {
+    const {name, value} = event.target;
+
+    setNewItem((preItem) => {
+      return {
+        ...preItem,
+        [name]: value
+      }
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleInputField} name="title" placeholder="Title" value={newItem.title} />
+        <textarea onChange={handleInputField} name="content" placeholder="Take a note..." rows="3" value={newItem.content} />
+        <button onClick={() => {
+          props.addItem(newItem);
+          setNewItem({
+            title: "",
+            content: ""
+          });
+        }}>Add</button>
       </form>
     </div>
   );
